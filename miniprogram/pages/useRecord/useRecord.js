879b4d8148707a96c5694a76f8d38c46248656b8
jessie <1185922395@qq.com>
2020-10-21 20:13:39 +0800

    第二个版本更改完成

--- miniprogram/pages/useRecord/useRecord.js
+++ miniprogram/pages/useRecord/useRecord.js
@@ -1,13 +1,86 @@
-// miniprogram/pages/useRecord/useRecord.js
+import Model from '../../model/model'
+import {formatDate} from '../../utils/util'
+
+const model = new Model()
+
 Page({
   data: {
-    allTime: 520,
-    create: 20,
-    delete: 0,
-    share: 0,
-    list: [
-      {title: '今天吃什么？', time: '5'},
-      {title: '今天吃什么？', time: '5'}
-    ]
+    list: [],
+    isEmpty: false,
+    showLoading: true,
+    tip: '加载中...'
+  },
+
+  async cleanHistory() {
+    const that = this
+    wx.showModal({
+      title: '提示',
+      content: '确认清除所有历史记录吗？',
+    }).then((res) => {
+      if (res.confirm) {
+        this.setData({showLoading: true, tip: '清空中...'})
+        const result = model.cleanHistory()
+        return result
+      } else if (res.cancel) {
+        return false
+      }
+    }).then((res) => {
+      if (res.result) {
+        that.getData()
+      }
+    })
+  },
+
+  async getData() {
+    const {data} = await model.getUserInfo()
+    const list = data[0].history
+
+    if (list.length !== 0) {
+      list.forEach(item => {
+        item.date = formatDate(item.date)
+      })
+    }
+    this.setData({
+      list,
+      isEmpty: list.length === 0
+    })
+    this.setData({showLoading: false})
+  },
+
+  onLoad() {
+    this.getData()
+  },
+
+  async onPullDownRefresh() {
+    wx.showNavigationBarLoading()
+    this.setData({showLoading: true, tip: '刷新中...'})
+    this.getData()
+    wx.hideNavigationBarLoading()
+    wx.stopPullDownRefresh()
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage(e) {
+    const {list} = this.data
+    let index = -1
+    let title = ''
+    let path = ''
+    if (e.from === 'button') {
+      index = e.target.dataset.index
+      const target = list[index]
+      title = `我在${target.title}里选中了${target.result}`
+      path = `/pages/home/home?id=${target._id}&checkIndex=${target.checkIndex}`
+    }
+    
+    return {
+      title,
+      path,
+      imageUrl: '../../images/shareImg.jpg',
+      success: function (res) {
+        console.log('成功', res)
+      }
+    }
   }
 })
